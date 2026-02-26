dd6e2431c993438a62040c10d363512f13cee4cf
Manika Dahal <manikadahal8@gmail.com>
2026-02-26 13:41:12 +0545

    Changed package name to publsh app in aptoide

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.chess_python"
+    namespace = "com.manikadahal.chessgame"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.chess_python"
+        applicationId = "com.manikadahal.chessgame"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(flutter.minSdkVersion) 
